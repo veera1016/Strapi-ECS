9a5369ca018b6161fd9f83342a89b81489659d2c
veera1016 <125983435+veera1016@users.noreply.github.com>
2024-07-03 16:59:39 +0530

    Update route53.tf

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -3,15 +3,16 @@ resource "aws_route53_zone" "main" {
 }
 
 resource "aws_route53_record" "strapi" {
-  zone_id = Z06607023RJWXGXD2ZL6M
-  name    = "togaruashok1996-ecs"  # Replace with your desired subdomain
+  zone_id = "Z06607023RJWXGXD2ZL6M"  # Replace with your actual hosted zone ID
+  name    = "togaruashok1996"  # Replace with your desired subdomain
   type    = "A"
   alias {
-    name                   = aws_ecs_service.strapi.endpoint
-    zone_id                = Z06607023RJWXGXD2ZL6M
+    name                   = aws_ecs_service.strapi.network_configuration[0].assign_public_ip
+    zone_id                = "Z06607023RJWXGXD2ZL6M"  # Replace with your actual hosted zone ID
     evaluate_target_health = true
   }
 }
+
 output "route53_zone_id" {
   value = aws_route53_zone.main.id
 }
