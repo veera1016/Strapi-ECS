23395cb0730bbd5ff772893257f7760a4aa9a0c4
veera1016 <125983435+veera1016@users.noreply.github.com>
2024-07-03 19:13:50 +0530

    Update ecs.tf

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -18,13 +18,29 @@ resource "aws_iam_role" "ecs_execution_role_new" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-  role       = aws_iam_role.ecs_execution_role_new.name
+resource "aws_iam_role" "ecs_execution_role_new_unique" {
+  name = "ecs_execution_role_new_unique"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      },
+    ]
+  })
 }
 
-resource "aws_iam_role" "ecs_task_role_new" {
-  name = "ecs_task_role_new"
+resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  role       = aws_iam_role.ecs_execution_role_new_unique.name
+}
+
+resource "aws_iam_role" "ecs_task_role_new_unique" {
+  name = "ecs_task_role_new_unique"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -40,9 +56,9 @@ resource "aws_iam_role" "ecs_task_role_new" {
 }
 
 resource "aws_ecs_task_definition" "strapi" {
-  family                   = "Ashok-strapi-task"
-  execution_role_arn       = aws_iam_role.ecs_execution_role_new.arn
-  task_role_arn            = aws_iam_role.ecs_task_role_new.arn
+  family                   = "strapi-task"
+  execution_role_arn       = aws_iam_role.ecs_execution_role_new_unique.arn
+  task_role_arn            = aws_iam_role.ecs_task_role_new_unique.arn
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "512"
