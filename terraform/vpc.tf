2ba2c78670500bd203c21028ac22a879dfa17ad1
veera1016 <125983435+veera1016@users.noreply.github.com>
2024-07-03 16:52:51 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,50 +1,31 @@
-# Create VPC
-resource "aws_vpc" "main" {
+resource "aws_vpc" "this" {
   cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
-  tags = {
-    Name = "main_vpc"
-  }
 }
 
-# Create a Subnet
-resource "aws_subnet" "main" {
-  vpc_id                  = aws_vpc.main.id
+resource "aws_subnet" "this" {
+  vpc_id                  = aws_vpc.this.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "main_subnet"
-  }
+  availability_zone       = "us-east-1"  # Change as needed
 }
 
-# Create Security Group
-resource "aws_security_group" "allow_all" {
-  vpc_id = aws_vpc.main.id
-
+resource "aws_security_group" "ecs" {
+  vpc_id = aws_vpc.this.id
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-    ingress {
+  ingress {
     from_port   = 1337
     to_port     = 1337
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_all"
   }
 }
