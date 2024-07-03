112502b8170e1242aeed10d6551ba119466de10b
veera1016 <125983435+veera1016@users.noreply.github.com>
2024-07-03 15:25:57 +0530

    Create ecs.tf

---
+++ terraform/ecs.tf
@@ -0,0 +1,54 @@
+resource "aws_ecs_task_definition" "strapi" {
+  family                   = "strapi-task"
+  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "512"
+  memory                   = "1024"
+
+  container_definitions = jsonencode([{
+    name      = "strapi"
+    image     = "veera1016/strapi:1.0.0"  # Replace with your Docker image
+    essential = true
+    portMappings = [
+      {
+        containerPort = 1337
+        hostPort      = 1337
+      }
+    ]
+  }])
+}
+
+resource "aws_ecs_service" "strapi" {
+  name            = "strapi-service"
+  cluster         = aws_ecs_cluster.this.id
+  task_definition = aws_ecs_task_definition.strapi.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+  network_configuration {
+    subnets          = [aws_subnet.this.id]
+    security_groups  = [aws_security_group.ecs.id]
+    assign_public_ip = true
+  }
+}
+
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ecs_execution_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  role     = aws_iam_role.ecs_execution_role.name
+}
