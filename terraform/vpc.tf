7d18b473587da30c3c4532f59bce33d85e41febe
veera1016 <125983435+veera1016@users.noreply.github.com>
2024-07-03 17:45:12 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,28 +1,22 @@
 # Existing VPC
-data "aws_vpc" "existing" {
+data "aws_vpc" "default" {
   default = true
 }
 
 # Subnet in existing VPC
 resource "aws_subnet" "this" {
-  vpc_id            = data.aws_vpc.existing.id
+  vpc_id            = data.aws_vpc.default.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-west-2a"
+  availability_zone = "us-east-a"
 }
 
 # Security Group
 resource "aws_security_group" "ecs" {
-  vpc_id = data.aws_vpc.existing.id
+  vpc_id = data.aws_vpc.default.id
 
   ingress {
     from_port   = 1337
     to_port     = 1337
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-    ingress {
-    from_port   = 80
-    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
