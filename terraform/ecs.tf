b1938dc38908da84050cfa4502b586946c6ec806
veera1016 <125983435+veera1016@users.noreply.github.com>
2024-07-03 17:17:51 +0530

    Update ecs.tf

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,5 +1,5 @@
 resource "aws_ecs_cluster" "this" {
-  name = "strapi-cluster"
+  name = "Ashok-strapi-cluster"
 }
 
 resource "aws_iam_role" "ecs_execution_role" {
@@ -62,7 +62,7 @@ resource "aws_ecs_task_definition" "strapi" {
 }
 
 resource "aws_ecs_service" "strapi" {
-  name            = "strapi-service"
+  name            = "Ashok-strapi-service"
   cluster         = aws_ecs_cluster.this.id
   task_definition = aws_ecs_task_definition.strapi.arn
   desired_count   = 1
