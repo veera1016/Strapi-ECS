486f40e35ae0d660a4b73db562d878b036781d89
veera1016 <125983435+veera1016@users.noreply.github.com>
2024-07-03 17:36:01 +0530

    Update ecs.tf

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,9 +1,23 @@
+# ECS Cluster
 resource "aws_ecs_cluster" "this" {
   name = "Ashok-strapi-cluster"
 }
 
-resource "aws_iam_role" "ecs_execution_role" {
+# Data sources for existing IAM roles
+data "aws_iam_role" "ecs_execution_role" {
   name = "ecs_execution_role"
+  depends_on = [aws_iam_role.ecs_execution_role]
+}
+
+data "aws_iam_role" "ecs_task_role" {
+  name = "ecs_task_role"
+  depends_on = [aws_iam_role.ecs_task_role]
+}
+
+# ECS Execution Role
+resource "aws_iam_role" "ecs_execution_role" {
+  count = length(data.aws_iam_role.ecs_execution_role.id) == 0 ? 1 : 0
+  name  = "ecs_execution_role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -18,13 +32,16 @@ resource "aws_iam_role" "ecs_execution_role" {
   })
 }
 
+# Attach Execution Policy
 resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-  role       = aws_iam_role.ecs_execution_role.name
+  role       = coalesce(aws_iam_role.ecs_execution_role[0].name, data.aws_iam_role.ecs_execution_role.name)
 }
 
+# ECS Task Role
 resource "aws_iam_role" "ecs_task_role" {
-  name = "ecs_task_role"
+  count = length(data.aws_iam_role.ecs_task_role.id) == 0 ? 1 : 0
+  name  = "ecs_task_role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -39,10 +56,11 @@ resource "aws_iam_role" "ecs_task_role" {
   })
 }
 
+# ECS Task Definition
 resource "aws_ecs_task_definition" "strapi" {
   family                   = "strapi-task"
-  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
-  task_role_arn            = aws_iam_role.ecs_task_role.arn
+  execution_role_arn       = coalesce(aws_iam_role.ecs_execution_role[0].arn, data.aws_iam_role.ecs_execution_role.arn)
+  task_role_arn            = coalesce(aws_iam_role.ecs_task_role[0].arn, data.aws_iam_role.ecs_task_role.arn)
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "512"
@@ -61,6 +79,7 @@ resource "aws_ecs_task_definition" "strapi" {
   }])
 }
 
+# ECS Service
 resource "aws_ecs_service" "strapi" {
   name            = "Ashok-strapi-service"
   cluster         = aws_ecs_cluster.this.id
