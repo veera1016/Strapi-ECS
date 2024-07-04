2cce7ede57bc43c5cf0960b3adb9c1eddccc726d
veera1016 <125983435+veera1016@users.noreply.github.com>
2024-07-04 14:06:29 +0530

    Create vpc.tf

---
+++ terraform/vpc.tf
@@ -0,0 +1,37 @@
+resource "aws_vpc" "this" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "this" {
+  vpc_id     = aws_vpc.this.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_security_group" "ecs" {
+  name_prefix = "ecs-security-group"
+  vpc_id      = aws_vpc.this.id
+
+  ingress {
+    from_port   = 1337
+    to_port     = 1337
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_eip" "strapi" {
+  vpc = true
+}
