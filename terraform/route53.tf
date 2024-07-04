ff4d86a1f509b5eee1728b351f1394a0778e93e1
veera1016 <125983435+veera1016@users.noreply.github.com>
2024-07-04 16:48:19 +0530

    Update route53.tf

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,13 +1,7 @@
-# Create a Route 53 hosted zone (if you don't have one)
-resource "aws_route53_zone" "example" {
-  name = "contentecho.in."  # Replace with your domain name
-}
-
-# Create a Route 53 A record pointing to your ECS service
 resource "aws_route53_record" "example" {
   zone_id = Z06607023RJWXGXD2ZL6M
   name    = "togaruashok1996.contentecho.in"  # Replace with your subdomain
   type    = "A"
   ttl     = "300"
-  records = [aws_ecs_service.example.network_configuration[0].assign_public_ip]  # Or a fixed IP if you're using a static IP setup
+  records = [aws_eip.example.public_ip]
 }
