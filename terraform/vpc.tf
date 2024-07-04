0f9fed5893f61bd26c810e3f4b2ce6b5ecc1b2f1
veera1016 <125983435+veera1016@users.noreply.github.com>
2024-07-04 16:11:30 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -17,12 +17,6 @@ resource "aws_security_group" "ecs" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   egress {
     from_port   = 0
@@ -33,5 +27,5 @@ resource "aws_security_group" "ecs" {
 }
 
 resource "aws_eip" "strapi" {
-  vpc = true
+  domain = "vpc"
 }
