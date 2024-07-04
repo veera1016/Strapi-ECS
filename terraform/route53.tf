3c6a4b714f81a81fe64c163b5a5b28e1adcd2a7b
veera1016 <125983435+veera1016@users.noreply.github.com>
2024-07-04 15:48:04 +0530

    Update route53.tf

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,15 +1,8 @@
-resource "aws_route53_zone" "main" {
-  name = "contentecho.in"  # Replace with your domain
-}
-
-resource "aws_route53_record" "strapi" {
-  zone_id = aws_route53_zone.main.id
-  name    = "togaruashok1996Ecs"  # Replace with your desired subdomain
+# Route 53 DNS record for sub-domain
+resource "aws_route53_record" "strapi_subdomain" {
+  zone_id = "Z06607023RJWXGXD2ZL6M"
+  name    = "togaruashok1996.contentecho.in"
   type    = "A"
-  ttl     = 300
-  records = [aws_eip.strapi.public_ip]
-}
-
-output "route53_zone_id" {
-  value = aws_route53_zone.main.id
+  ttl = "300"
+  records = [aws_eip.strapi_service1_ip.public_ip]   
 }
